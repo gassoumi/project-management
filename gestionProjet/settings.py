917d419c9ae2ee0866d20eb30afcd36beda475b6
achref <gassoumi_achraf@live.fr>
2021-06-02 18:51:48 +0100

    first commit

--- gestionProjet/settings.py
+++ gestionProjet/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dbTest',
         'USER': 'postgres',
-        'PASSWORD': 'magic2005',
+        'PASSWORD': '*****',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
